d657ad4871dcc1e8b6d723393f9553415ff71dab
CrazyLegumes <deepwns@live.com>
2016-12-05 02:22:41 -0600

    Added Menus
    Added scripts and prefabs for menus

---
+++ 327MainCorse/Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+    
+
+
+	// Use this for initialization
+	void Start ()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Debug.Log(GameManager.Instance.state);
+	}
+
+
+    public void StartLevel()
+    {
+        SceneManager.LoadScene(1);
+        Time.timeScale = 1f;
+        GameManager.Instance.state = game.game;
+        //Change game manager state here
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+        GameManager.Instance.state = game.mainmenu;
+        //Change game manager state here
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+        GameManager.Instance.state = game.credits;
+        //Change game manager state here
+    }
+
+    public void RestartLevel()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        FindObjectOfType<PlayerController>().Transition(gamestate.init);
+        
+        GameManager.Instance.state = game.game;
+        //Change game manager state here
+    }
+
+}
--- 327MainCorse/Assets/Scripts/PlayerController.cs
+++ 327MainCorse/Assets/Scripts/PlayerController.cs
@@ -166,7 +166,7 @@ public class PlayerController : MonoBehaviour
 
     }
 
-    void Transition(gamestate newstate)
+    public void Transition(gamestate newstate)
     {
         if (states.Contains(new KeyValuePair<gamestate, gamestate>(state, newstate)))
         {
